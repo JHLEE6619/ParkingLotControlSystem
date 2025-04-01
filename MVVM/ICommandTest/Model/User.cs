95bc2022d1514e65911d4efea9c3f2b56b9cd502
LJH <ljh4989@gmail.com>
2025-04-01 19:35:00 +0900

    control 배치중

---
+++ MVVM/ICommandTest/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICommandTest.Model
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Pw { get; set; }
+    }
+}
---
+++ MVVM/ICommandTest/ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ICommandTest.Model;
+using ICommandTest.ViewModel.Commands;
+
+namespace ICommandTest.ViewModel
+{
+    public class MainViewModel
+    {
+        // View의 코드 비하인드에서
+        // 버튼 클릭 이벤트 핸들러에 작성한 로직을
+        // ViewModel에서 함수로 구현합니다.
+        public User user { get; set; } = new User() 
+        {
+            Id = "ID",
+            Pw = "PW"
+        };
+        public Command Cmd { get; set; }
+        public MainViewModel() 
+        {
+            Cmd = new Command(DisplayMessage);
+        }
+        public void DisplayMessage()
+        {
+            MessageBox.Show($"아이디 : {user.Id}, 비밀번호 : {user.Pw}");
+        }
+    }
+}
